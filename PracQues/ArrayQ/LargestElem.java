1fc0376df8acc348dbad00f71bc16fe611f2655a
parnika0724 <parnika03066030@gmail.com>
2025-04-17 11:31:34 +0530

    Update

--- PracQues/ArrayQ/LargestElem.java
+++ PracQues/ArrayQ/LargestElem.java
@@ -7,7 +7,16 @@ public class LargestElem {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int len = sc.nextInt();
-        int array[] = new int[];
-
+        int array[] = new int[len];
+        for (int i = 0; i < len; i++) {
+            array[i] = sc.nextInt();
+        }
+        int largest = array[0];
+        for (int i = 0; i < len; i++) {
+            if (largest < array[i]) {
+                largest = array[i];
+            }
+        }
+        System.out.println(largest);
     }
 }
